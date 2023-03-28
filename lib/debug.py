afd4faedc2a122f6ff8575d8127371ae86b174c4
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-28 11:25:37 -0400

    table maybe

--- lib/debug.py
+++ lib/debug.py
@@ -1,1 +1,20 @@
-#please work
+from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer) 
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+
+class Store(Base):
+    __tablename__ = 'stores'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+
+    id = Column( Integer() )
+    name = Column( String() )
+    location = Column( String() )
+
+    # def __repr__(self):
+    #     return f"ID: { self.id }, " \
+    #     + f"Name: { self.name } " \
+    #     + f"Species: { self.location } "
