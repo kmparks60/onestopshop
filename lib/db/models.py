f5edb2cd5104c80611363e17df02fd106ded00ff
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-29 15:55:42 -0400

    foreign keys and table for stores set up

--- lib/db/models.py
+++ lib/db/models.py
@@ -30,8 +30,8 @@ class Customer(Base):
     supplies = relationship('Inventory', backref=('customers'))
 
     def __repr__(self):
-        return f'Customer(id={self.id}, ' + \
-            f'name={self.name}) ' \
+        return f'Customer(id={self.id}, ' \
+        + f'name={self.name}) ' \
 
 class Inventory(Base):
     __tablename__ = 'supplies'
@@ -40,10 +40,11 @@ class Inventory(Base):
     item = Column(String())
     price = Column(Integer())
     quantity = Column(Integer())
-    stores_id = Column(Integer(), ForeignKey('stores.id'))
-    customers_id = Column(Integer(), ForeignKey('customers.id'))
-    # stores = relationship('Store', backref='supplies')
-    # winery = relationship('Winery', backref='supplies')
+    stores_id = Column(Integer(), ForeignKey( Store.id ) )
+    customers_id = Column(Integer(), ForeignKey( Customer.id ) )
+
+    stores = relationship( 'Store', foreign_keys='Inventory.stores_id' )
+    customers = relationship( 'Customer', foreign_keys='Inventory.customers_id' )
 
     def __repr__(self):
         return f'Inventory(id={self.id}, ' \
--- lib/debug.py
+++ lib/debug.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from db.models import ( Base, Store, Customer, Inventory )
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///onestopshop.db')
+    engine = create_engine('sqlite:///db/onestopshop.db')
     Base.metadata.create_all( engine )
     Session = sessionmaker( bind=engine )
     session = Session()
