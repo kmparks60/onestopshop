46f051ff23f313410def51f428931da4607737b7
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-30 14:36:30 -0400

    began options for cli

--- lib/cli.py
+++ lib/cli.py
@@ -15,6 +15,27 @@ class CLI:
         print(f'***** Welcome to the Shop-O-Rama-Palooza *****')
         print(' ')
 
+        exit = False
+        while exit == False:
+            option = input(f'Enter "view stores" to see a list of our funded stores created by our signed owners, or enter "sign me" to become an owner today: ')
+            print('')
+            if option.lower() == "view stores":
+                show_stores( self )
+            elif option.lower() == "sign me":
+                sign_contract( self )
+            
+def show_stores( self ):
+    print("stores showing here")
+    print("")
+
+def sign_contract( self ):
+    print("contract be here")
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
