afb087d13ec12ac861a2832ae4691e3cee7b44a1
Matthew Longshore <matthewlongshore12@gmail.com>
2023-03-31 00:48:41 -0400

    Added if else and add functionality

--- lib/cli.py
+++ lib/cli.py
@@ -9,19 +9,41 @@ class CLI:
         self.owners = [ owner for owner in session.query( Owner ) ]
         self.supplies = [ inventory for inventory in session.query( Inventory ) ]
         self.name = user_input
-        self.begin()
+        if any(owner.name == user_input for owner in self.owners):
+            print(' ')
+            print(f'<<<<< Welcome Back {user_input} >>>>>')
+            print('           ||||||||||||||             ')
+            print('      <<<<< OWNER PORTAL >>>>>    ')
+            print('           vvvvvvvvvvvvvv            ')
+            print(' ')
+
+            exit = False
+            while exit == False:
+                option = input(f'If you would like to add an item to your inventory enter "add"\nIf you would like to view other owners or stores enter "view"\nTo Exit at any time Type "exit"\nEnter Text Here ==> ')
+                print('')
+                if option.lower() == "add":
+                    add_inventory( self )
+                elif option.lower() == "view":
+                    show_stores( self )
+                elif option.lower() == "exit":
+                    return        
+        else:
+            self.begin()
 
     def begin( self ):
         print(' ')
-        print(f'***** Welcome to the Shop-O-Rama-Palooza *****')
+        print('***** Welcome to the Shop-O-Rama-Palooza *****')
+        print('         <<<<< GUEST PORTAL >>>>>          ')
         print(' ')
 
         exit = False
         while exit == False:
-            option = input(f'To Get Started Type "start"\nEnter Text Here ==> ')
+            option = input(f'To Get Started Type "start"\nTo Exit at any time Type "exit"\nEnter Text Here ==> ')
             print('')
             if option.lower() == "start":
                 show_stores( self )
+            elif option.lower() == "exit":
+                return
             # elif option.lower() == "sign me":
             #     sign_contract( self )
             
@@ -98,8 +120,33 @@ def owners_list(owners):
         print("3. Joystick Junction")
         print("")
 
-def sign_contract( self ):
-    print("contract be here")
+
+
+def add_inventory( self ):
+    add_item = input("Enter the item you would like to add: ")
+    add_price = input("Enter the price of this item: ")
+    same_store_id = input("Enter the id of your store: ")
+    same_owner_id = input("Enter your owner id for confirmation: ")
+
+    added_inventory = Inventory(
+        item = add_item,
+        price = add_price,
+        stores_id = same_store_id,
+        owners_id = same_owner_id
+    )
+
+    session.add( added_inventory )
+    session.commit()
+
+    self.supplies.append( added_inventory )
+    print('')
+    print(f'Thank you {user_input}, your item has been added!')
+    print('')
+
+
+
+# def sign_contract( self ):
+#     print("contract be here")
 
 
 
@@ -112,5 +159,5 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///onestopshop.db')
     Session = sessionmaker(bind=engine)
     session = Session()
-    user_input = input("\nPress Enter Key To Get Started\n==>")
+    user_input = input("\nWelcome!\nIf you are an owner type your full name to access the OWNER PORTAL\nPress Enter if you would like to enter the GUEST PORTAL\n==> ")
     CLI(user_input)
